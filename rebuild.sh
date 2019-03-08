44e0ab211cf9c11f39f70cbaa20def2ec2fad68f
sugartom <sugartom.hu@gmail.com>
2019-03-07 18:20:47 -0800

    added rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+sudo pip uninstall -y protobuf
+sudo pip uninstall -y tensorflow
+bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip install /tmp/tensorflow_pkg/tensorflow-1.12.0-cp27-cp27mu-linux_x86_64.whl
